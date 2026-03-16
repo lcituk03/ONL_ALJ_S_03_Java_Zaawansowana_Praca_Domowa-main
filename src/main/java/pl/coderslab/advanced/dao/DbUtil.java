5e0f01c6bcaf3e453892e4d8959453fb6b4d3320
lcituk03 <lcituk@visa.com>
2026-03-16 18:40:38 +0100

    Update DbUtil.java

--- src/main/java/pl/coderslab/advanced/dao/DbUtil.java
+++ src/main/java/pl/coderslab/advanced/dao/DbUtil.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DbUtil {
 	private static final String DB_URL = "jdbc:mysql://localhost:3306/dao_pd?useSSL=false";
 	private static final String DB_USER = "root";
-	private static final String DB_PASS = "V1i2s3a4<3me";
+	private static final String DB_PASS = "e";
 
 	public static Connection getConnection() throws SQLException {
 		return DriverManager.getConnection(DB_URL, DB_USER, DB_PASS);
